62d2e3e30871cb285ba048d578cf92635e80c19b
Jason Lough <jason.lough@gmail.com>
2018-04-10 15:51:35 -0700

    Update main.js

--- node_emitter_proj/main.js
+++ node_emitter_proj/main.js
@@ -1,12 +1,5 @@
 /*
   This is the starting point for this experiment.
-  
-  Objectives:
-  Understand how to create, import, and use our own package in node_modules
-  Create a simple custom dynamic event handler using Nodes events.EventEmitter
-  Create an emitter with chainable functions so you can do stuff like:
-      a.b().b().b() etc...
-  
 */
 
 'use strict'
